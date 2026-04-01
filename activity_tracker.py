b7d1fab87f21341e9b16dbd61120ea59579d880d
Levent <leventcakmk@gmail.com>
2026-04-01 17:17:17 +0300

    Fix: inactive periods key collision with ip_address (ip → inp)

--- activity_tracker.py
+++ activity_tracker.py
@@ -1351,7 +1351,7 @@ class OptimizedDataSyncer:
         
         # Add only the valid inactive periods (within batch window)
         if valid_inactive_periods:
-            optimized_data['ip'] = valid_inactive_periods
+            optimized_data['inp'] = valid_inactive_periods
         
 
         
